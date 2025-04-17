134bf81fe5ac6d0d71715b001f0379d4861b217f
Likepint <jsuuu96@naver.com>
2025-04-17 15:25:39 +0900

    바킹독 Queue 문제집

---
+++ basic-algo-workbook/Baekjoon/BaaaaaaaarkingDog/Queue/2164.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+#define endl '\n'
+
+int n;
+queue<int> q;
+
+void Excute()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+		q.push(i);
+
+	while (q.size() > 1)
+	{
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front() << endl;
+}
+
+int main()
+{
+	Excute();
+
+	return 0;
+}
