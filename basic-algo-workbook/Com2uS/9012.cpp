905c8f433a00bf62a6c49b5aa062c20e2edb4594
Likepint <jsuuu96@naver.com>
2025-05-19 11:42:25 +0900

    baekjoon 9012 Parenthesis

---
+++ basic-algo-workbook/Com2uS/9012.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+#define endl '\n'
+
+int T;
+
+bool IsVPS(const string& InStr)
+{
+    stack<char> ps;
+
+    for (char ch : InStr)
+    {
+        if (ch == '(')
+        {
+            ps.push(ch);
+        }
+        else // ch == ')'
+        {
+            if (ps.empty())
+                return false;
+
+            ps.pop();
+        }
+    }
+
+    return ps.empty();
+}
+
+int main()
+{
+    cin >> T;
+
+    for (int i = 0; i < T; ++i)
+    {
+        string input;
+        cin >> input;
+
+        cout << (IsVPS(input) ? "YES" : "NO") << endl;
+    }
+
+    return 0;
+}
