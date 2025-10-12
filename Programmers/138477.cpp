694ce17467c43fa9cec5c850f1d5f1544a93ef82
likepint <jsuuu96@kakao.com>
2025-10-13 00:48:11 +0900

    프로그래머스 명예의 전당 (1)

---
+++ Programmers/138477.cpp
@@ -0,0 +1,40 @@
+﻿// 명예의 전당 (1)
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+#define endl '\n'
+
+vector<int> solution(int k, vector<int> score)
+{
+    vector<int> answer;
+
+    priority_queue<int, vector<int>, greater<int>> Honors;
+
+    for (int i = 0; i < score.size(); ++i)
+    {        
+        Honors.push(score[i]);
+
+        if (Honors.size() > k)
+            Honors.pop();
+            
+        answer.emplace_back(Honors.top());
+    }
+
+    return answer;
+}
+
+int main()
+{
+    int k = 4;
+    vector<int> score = { 0, 300, 40, 300, 20, 70, 150, 50, 500, 1000 };
+
+    vector<int> answer = solution(k, score);
+    for (const auto& scores : answer)
+        cout << scores << endl;
+
+    return 0;
+}
