c2d97da6698206cac5dd81bfceab3177bd1052a9
likepint <jsuuu96@kakao.com>
2025-11-03 22:00:56 +0900

    Update basic-algo-workbook

--- Global.h
+++ Global.h
@@ -1,3 +1,14 @@
 ﻿#include <iostream>
+#include <vector>
+
+using namespace std;
 
 #define endl '\n'
+
+inline void Print(const vector<int>& InList)
+{
+    for (const auto& list : InList)
+        cout << list << " ";
+
+    cout << endl;
+}
